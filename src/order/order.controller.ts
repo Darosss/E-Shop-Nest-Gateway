bc0ef7a3f1d9350e6c04d31a7444d900d36b0a01
Darosss <daroing12@gmail.com>
2023-07-15 12:45:37 +0200

    initial commit

---
+++ src/order/order.controller.ts
@@ -0,0 +1,42 @@
+import {
+  Controller,
+  Inject,
+  Post,
+  OnModuleInit,
+  UseGuards,
+  Req,
+} from '@nestjs/common';
+import { ClientGrpc } from '@nestjs/microservices';
+import { Observable } from 'rxjs';
+import {
+  CreateOrderResponse,
+  OrderServiceClient,
+  ORDER_SERVICE_NAME,
+  CreateOrderRequest,
+} from './pb/order.pb';
+import { AuthGuard } from '../auth/guard/auth.guard';
+import { Request } from 'express';
+
+@Controller('order')
+export class OrderController implements OnModuleInit {
+  private svc: OrderServiceClient;
+
+  @Inject(ORDER_SERVICE_NAME)
+  private readonly client: ClientGrpc;
+
+  public onModuleInit(): void {
+    this.svc = this.client.getService<OrderServiceClient>(ORDER_SERVICE_NAME);
+  }
+
+  @Post()
+  @UseGuards(AuthGuard)
+  private async createOrder(
+    @Req() req: Request,
+  ): Promise<Observable<CreateOrderResponse>> {
+    const body: CreateOrderRequest = req.body;
+
+    body.userId = <number>req.user;
+
+    return this.svc.createOrder(body);
+  }
+}

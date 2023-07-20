d0c680d2d233b19e3e51f6f553dcc6d3ba23b709
Darosss <daroing12@gmail.com>
2023-07-20 12:02:46 +0200

    feat: add AllExceptionFilter to useGlobalFilters

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,13 @@
-import { NestFactory } from '@nestjs/core';
+import { HttpAdapterHost, NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { AllExceptionsFilter } from './filters/all-exceptions.filter';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const httpAdapterHost = app.get(HttpAdapterHost);
+  app.useGlobalFilters(new AllExceptionsFilter(httpAdapterHost));
+
   await app.listen(3000);
 }
 bootstrap();

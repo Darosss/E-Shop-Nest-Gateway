cb161df53c24ccf1b7d236728afe3bd055f6871b
Darosss <daroing12@gmail.com>
2023-07-20 12:02:24 +0200

    feat: add CategoryModule to imports

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,8 +2,9 @@ import { Module } from '@nestjs/common';
 import { AuthModule } from './auth/auth.module';
 import { ProductModule } from './product/product.module';
 import { OrderModule } from './order/order.module';
+import { CategoryModule } from './category/category.module';
 
 @Module({
-  imports: [AuthModule, ProductModule, OrderModule],
+  imports: [AuthModule, ProductModule, OrderModule, CategoryModule],
 })
 export class AppModule {}

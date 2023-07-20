95a85f09e1bbdf7595f3c7eb1690841850911869
Darosss <daroing12@gmail.com>
2023-07-20 13:58:22 +0200

    feat: add updateProduct PATCH endpoint

--- src/product/product.controller.ts
+++ src/product/product.controller.ts
@@ -8,6 +8,7 @@ import {
   UseGuards,
   Post,
   Body,
+  Patch,
 } from '@nestjs/common';
 import { ClientGrpc } from '@nestjs/microservices';
 import { Observable } from 'rxjs';
@@ -17,7 +18,8 @@ import {
   PRODUCT_SERVICE_NAME,
   ProductOperationResponse,
   CreateProductRequest,
-} from './pb/product.pb';
+  UpdateProductRequest,
+} from '../pb/product.pb';
 import { AuthGuard } from '../auth/guard/auth.guard';
 
 @Controller('product')
@@ -38,6 +40,15 @@ export class ProductController implements OnModuleInit {
     @Body() body: CreateProductRequest,
   ): Promise<Observable<ProductOperationResponse>> {
     return this.svc.createProduct(body);
+  }
+
+  @Patch(':id')
+  @UseGuards(AuthGuard)
+  private async updateProduct(
+    @Param('id', ParseIntPipe) id: number,
+    @Body() body: UpdateProductRequest,
+  ): Promise<Observable<ProductOperationResponse>> {
+    return this.svc.updateProduct({ id: id, ...body });
   }
 
   @Get(':id')

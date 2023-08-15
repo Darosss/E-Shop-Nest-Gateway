579aa182574705b05413ec058393971fab007680
Darosss <daroing12@gmail.com>
2023-08-15 14:15:35 +0200

    feat: import reflect-metadata for class-validator

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import { HttpAdapterHost, NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { AllExceptionsFilter } from './filters/all-exceptions.filter';
+import 'reflect-metadata';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, { cors: true });

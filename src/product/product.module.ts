09cd514dab146cce78fdb2a9732b6e229946eff3
Darosss <daroing12@gmail.com>
2023-10-06 02:11:10 +0200

    feat: add Property, PropertyCategory, ProductProperty modules to imports

--- src/product/product.module.ts
+++ src/product/product.module.ts
@@ -2,6 +2,9 @@ import { Module } from '@nestjs/common';
 import { ClientsModule, Transport } from '@nestjs/microservices';
 import { PRODUCT_PACKAGE_NAME, PRODUCT_SERVICE_NAME } from '../pb/product.pb';
 import { ProductController } from './product.controller';
+import { PropertyModule } from './property/property.module';
+import { PropertyCategoryModule } from './property-category/property-category.module';
+import { ProductPropertyModule } from './product-property/product-property.module';
 
 @Module({
   imports: [
@@ -11,11 +14,15 @@ import { ProductController } from './product.controller';
         transport: Transport.GRPC,
         options: {
           url: '0.0.0.0:50053',
-          package: PRODUCT_PACKAGE_NAME,
+          package: [PRODUCT_PACKAGE_NAME],
           protoPath: 'node_modules/e-shop-nest-proto/proto/product.proto',
         },
       },
     ]),
+
+    PropertyModule,
+    PropertyCategoryModule,
+    ProductPropertyModule,
   ],
   controllers: [ProductController],
 })

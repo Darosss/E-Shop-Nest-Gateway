184742aa1f34d5fea4da2cb3b95dfc505d340581
Darosss <daroing12@gmail.com>
2023-10-07 12:07:27 +0200

    feat: add import dotenv/config;

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,4 @@
+import 'dotenv/config';
 import { HttpAdapterHost, NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { AllExceptionsFilter } from './filters/all-exceptions.filter';
